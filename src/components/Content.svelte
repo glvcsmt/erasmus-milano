f9af836eda328c7f5d2acff65bb8327da225621c
Szilviniki <szilviniki9@gmail.com>
2023-02-08 17:41:04 +0100

    Content Component

--- src/components/Content.svelte
+++ src/components/Content.svelte
@@ -2,5 +2,22 @@
 
 </script>
 <main>
-    
+  <div>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus et magna sodales, mattis ligula id, imperdiet metus. Suspendisse potenti. Donec nec nibh ante. Aenean vel sem nisl. Proin ac velit tortor. Suspendisse id turpis sed leo accumsan semper. Proin tristique feugiat mi, mollis pellentesque sapien aliquet et. Duis dictum molestie massa. Mauris vitae venenatis velit. Curabitur mollis metus quis neque pellentesque, at accumsan sem ornare.
+
+Nulla imperdiet nibh sit amet ipsum elementum, eget laoreet magna consequat. Proin fermentum libero eu luctus imperdiet. Sed tempor vitae mauris id egestas. Aenean ac arcu vel arcu consequat euismod ut ac nisl. Pellentesque efficitur semper volutpat. Donec ac ligula a mi aliquet porta non in odio. Cras fringilla justo eget metus porttitor, sed accumsan massa venenatis.
+
+Maecenas gravida justo sem, non tristique nulla dignissim vel. Morbi in consequat sem, non suscipit quam. Nulla facilisi. Cras felis lectus, auctor vel quam et, varius gravida mi. Proin cursus tristique quam a scelerisque. Fusce ut enim lacus. Integer non sollicitudin odio. Pellentesque consectetur massa molestie tempor suscipit. Donec hendrerit malesuada tincidunt. Integer ac tempor sem.
+    </p>
+  </div>  
 </main>
+<style>
+    div{
+       margin-left: 18%; 
+       margin-right: 18%;
+    }
+p{
+    text-align: justify;
+}
+</style>
